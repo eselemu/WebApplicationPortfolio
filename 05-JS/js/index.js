2f4e8f3edda92bd760a13d847774d13af9d36d3d
eselemu <119244920+eselemu@users.noreply.github.com>
2023-09-26 11:58:19 -0600

    +Css styling to css page

--- 05-JS/js/index.js
+++ 05-JS/js/index.js
@@ -1,4 +1,20 @@
-function namePepe(){
+document.querySelector("h1").addEventListener("click",() => {
     alert('Pepe es mi hijo y un pendejo');
-    document.getElementsByTagName("p")[0].innerHTML = "U are an idiot";
-}
+    //document.getElementsByTagName("p")[0].innerHTML = "U are an idiot";
+    var oopsArray = document.getElementsByClassName("oops");
+    oopsArray[0].innerHTML = "HOla";
+    //oopsArray[0].classList.add("special")
+    oopsArray[0].classList.toggle("special");
+    for(i = 0; i < oopsArray.length; i++){
+        oopsArray[i].textContent = "<b>Holapas</b>";
+    }
+
+    //Most of the times we use query selector
+    var liArray = document.querySelectorAll(".nums li");
+    for(i = 0; i < liArray.length; i++){
+        liArray[i].style = "color: blue";
+        liArray[i].style.fontSize = "2rem";
+    }
+
+    document.querySelector("a").setAttribute("href", "https://paletadecolores.online/colors/ffe59a/");
+});
