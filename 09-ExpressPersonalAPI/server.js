7e62bbb2c330f9f17b1606e33e1f402a8529515d
eselemu <119244920+eselemu@users.noreply.github.com>
2023-10-23 11:35:57 -0600

    Finished portfolio P2v01 +Added git ignore

--- 09-ExpressPersonalAPI/server.js
+++ 09-ExpressPersonalAPI/server.js
@@ -8,10 +8,11 @@ app.engine("ejs", require("ejs").renderFile);
 app.set("view engine", "ejs");
 
 const names = [];
+const tasks = [];
 
 app.route("/")
 .get((req, res) =>{
-    res.render("index", { names: names });
+    res.render("index", { names: names, tasks: tasks });
 })
 .post((req, res) =>{
     res.redirect("/");
@@ -23,12 +24,36 @@ app.route("/greet")
     names.push(name);
     console.log(name);
     res.redirect("/");
+})
+.put((req, res) =>{
+    var name = req.query.name;
+    names.push(name);
+    console.log(name);
+    res.status(200).json({ names: names });
 });
 
 app.route("/wazzup")
 .get((req, res) =>{
     var name = req.query.name;
     res.render("wazzup", { name: name });
+});
+
+app.route("/task")
+.post((req, res) =>{
+    var task = req.body.task;
+    tasks.push(task);
+    res.redirect("/");
+})
+.get((req, res) =>{
+    var index = req.query.index;
+    tasks.splice(index, 1);
+    res.redirect("/");
+})
+.delete((req, res) =>{
+    var index = req.query.index;
+    tasks.splice(index, 1);
+    console.log("Hellothere from delete");
+    res.redirect("/");
 });
 
 app.use((err, req, res, next)=>{
